83fefae335cd28950a4f037a5c6fc9e43075d07f
Jarema <git@jarema.me>
2024-07-31 13:25:20 +0700

    fix the sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import icon from "astro-icon";
@@ -11,6 +12,7 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap(),
+    svelte(),
     solidJs(),
     icon(),
     tailwind({ applyBaseStyles: false }),
