51c9edc9bd2a1678a42854703456596842df2aa1
Jarema <36879694+jartf@users.noreply.github.com>
2025-11-27 08:00:45 +0000

    update configs

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,6 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     unoptimized: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   async headers() {
     return [
       {
