3adf520083ccf4b163c1a1dcfe2ec3ccaa0bbf00
Jarema <36879694+jartf@users.noreply.github.com>
2025-11-27 09:34:13 +0000

    temp disable COEP for cross-origin resources

--- next.config.mjs
+++ next.config.mjs
@@ -36,7 +36,7 @@ const nextConfig = {
           },
           {
             key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            value: 'unsafe-none',
           },
           {
             key: 'Cross-Origin-Opener-Policy',
