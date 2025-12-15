27b9721a77932461aaa23f7da627df80e3cf3f70
Jarema <36879694+jartf@users.noreply.github.com>
2025-12-15 10:22:39 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -74,18 +74,15 @@ const nextConfig = {
             value: 'https://kagi.com',
           },
           {
-            key: 'clear-site-data',
-            value: '"storage"',
-          },
-          {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload',
           },
           {
             key: 'Content-Security-Policy',
             value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "default-src 'none'",
+              "manifest-src 'self'",
+              "script-src 'self' 'unsafe-inline'",
               "style-src 'self' 'unsafe-inline' fonts.googleapis.com",
               "font-src 'self' fonts.gstatic.com data:",
               "img-src 'self' data: https: blob: *.ytimg.com *.rcd.gg *.googleusercontent.com *.githubusercontent.com",
