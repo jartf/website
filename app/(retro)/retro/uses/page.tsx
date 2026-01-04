14f07c67687c0edd2f5c71112cb22e85aaa5509c
Jarema <36879694+jartf@users.noreply.github.com>
2026-01-04 16:14:02 +0000

    retro uses

--- app/(retro)/retro/uses/page.tsx
+++ app/(retro)/retro/uses/page.tsx
@@ -11,8 +11,8 @@ export default function RetroUsesPage() {
     <>
       {/* Page title */}
       <center>
-        <font size={5} color="#000080">
-          <b>🔧 What I Use</b>
+        <font size={5}>
+          <b>Uses</b>
         </font>
         <hr width="50%" />
         <font size={2}>
@@ -35,9 +35,9 @@ export default function RetroUsesPage() {
       {/* Uses categories */}
       {USES_CATEGORIES.map((category, catIndex) => (
         <div key={catIndex}>
-          <font size={4} color="#000080">
+          <font size={4}>
             <b>
-              🔧 {category.title.charAt(0).toUpperCase() + category.title.slice(1)}
+              {category.title.charAt(0).toUpperCase() + category.title.slice(1)}
             </b>
           </font>
 
@@ -46,7 +46,6 @@ export default function RetroUsesPage() {
             cellPadding={5}
             cellSpacing={2}
             style={{
-              backgroundColor: '#f0f0f0',
               marginBottom: '15px',
             }}
           >
@@ -54,9 +53,6 @@ export default function RetroUsesPage() {
               {category.items.map((item, itemIndex) => (
                 <tr
                   key={itemIndex}
-                  style={{
-                    backgroundColor: itemIndex % 2 === 0 ? '#ffffff' : '#f5f5f5',
-                  }}
                 >
                   <td width="30%" valign="top">
                     <font size={2}>
@@ -80,9 +76,6 @@ export default function RetroUsesPage() {
                 sub.items.map((item, itemIndex) => (
                   <tr
                     key={`${subIndex}-${itemIndex}`}
-                    style={{
-                      backgroundColor: itemIndex % 2 === 0 ? '#ffffff' : '#f5f5f5',
-                    }}
                   >
                     <td width="30%" valign="top">
                       <font size={2}>
@@ -110,35 +103,10 @@ export default function RetroUsesPage() {
 
       <hr />
 
-      {/* Uses.tech badge */}
-      <center>
-        <table
-          cellPadding={10}
-          style={{ backgroundColor: '#e8f5e9', border: '2px solid #4caf50' }}
-        >
-          <tbody>
-            <tr>
-              <td>
-                <center>
-                  <font size={2}>
-                    <b>📋 This is a /uses page!</b>
-                    <br />
-                    Learn more at{' '}
-                    <a href="https://uses.tech/">uses.tech</a>
-                  </font>
-                </center>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </center>
-
-      <br />
-
       {/* Back link */}
       <center>
         <font size={2}>
-          <a href="/retro">&lt;&lt; Back to Homepage</a>
+          <a href="/retro">&lt;&lt; Back to homepage</a>
         </font>
       </center>
     </>
