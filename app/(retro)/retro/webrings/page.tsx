740205e129c39526fb30634400a77f5dbf558592
Jarema <36879694+jartf@users.noreply.github.com>
2026-01-04 16:26:23 +0000

    retro webrings page

--- app/(retro)/retro/webrings/page.tsx
+++ app/(retro)/retro/webrings/page.tsx
@@ -12,12 +12,12 @@ export default function RetroWebringsPage() {
       {/* Page title */}
       <center>
         <font size={5} color="#000080">
-          <b>🕸️ Webrings</b>
+          <b>Webrings</b>
         </font>
         <hr width="50%" />
         <font size={2}>
           <i>
-            Webrings: the original way to discover cool websites on the World Wide Web!
+            They were the original way to discover cool websites on the World Wide Web!
           </i>
         </font>
       </center>
@@ -28,8 +28,7 @@ export default function RetroWebringsPage() {
         <font size={3}>
           <b>What are webrings?</b> Webrings are collections of websites linked together
           in a circular structure. They were incredibly popular in the 1990s as a way
-          to discover new websites with similar interests before search engines became
-          dominant.
+          to discover new websites with similar interests before search engines came to be.
         </font>
       </p>
 
@@ -49,7 +48,6 @@ export default function RetroWebringsPage() {
             cellPadding={10}
             cellSpacing={0}
             style={{
-              backgroundColor: index % 2 === 0 ? '#f5f5f5' : '#ffffff',
               border: '2px groove #808080',
               marginBottom: '10px',
             }}
@@ -63,6 +61,7 @@ export default function RetroWebringsPage() {
                         <a href={webring.url}>{webring.name}</a>
                       </b>
                     </font>
+                    <br />
                     {webring.description && (
                       <>
                         <br />
@@ -97,20 +96,14 @@ export default function RetroWebringsPage() {
       <table
         width="100%"
         cellPadding={15}
-        style={{ backgroundColor: '#e8f5e9', border: '3px ridge #4caf50' }}
+        style={{ border: '3px ridge #4caf50' }}
       >
         <tbody>
           <tr>
             <td>
               <center>
-                <font size={3} color="#2e7d32">
-                  <b>🌐 Start Your Own Webring Journey!</b>
-                </font>
-                <br />
-                <font size={2}>
-                  Click through the webrings above to discover amazing personal websites.
-                  <br />
-                  The spirit of the old web lives on!
+                <font size={3}>
+                  Choose one webring above and navigate through them to discover other cool websites!
                 </font>
               </center>
             </td>
