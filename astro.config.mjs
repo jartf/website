10cbbbe9a664df90672a25dcfc6a17d079c992ba
Jarema <36879694+jartf@users.noreply.github.com>
2026-01-11 22:37:14 +0000

    hm

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,32 @@ export default defineConfig({
   output: 'static',
   adapter: vercel(),
 
+  experimental: {
+    csp: {
+      directives: [
+        "default-src 'none'",
+        "connect-src 'self' jarema.me z.is-a.dev ws.audioscrobbler.com api-gateway.umami.dev cloud.umami.is cusdis.com vercel.live",
+        "font-src 'self'",
+        "frame-src jarema.atabook.org vercel.live",
+        "img-src 'self' https: *.ytimg.com *.rcd.gg *.googleusercontent.com *.githubusercontent.com moods.imood.com lastfm.freetls.fastly.net",
+        "manifest-src 'self'",
+        "media-src 'self'",
+        "object-src 'none'",
+        "worker-src 'none'",
+        "base-uri 'self'",
+        "form-action 'self'",
+        "frame-ancestors 'self' geekring.net kagi.com",
+        'upgrade-insecure-requests',
+      ],
+      scriptDirective: {
+        resources: ["'self'", 'cusdis.com', 'cloud.umami.is', 'https://vercel.live'],
+      },
+      styleDirective: {
+        resources: ["'self'", 'cusdis.com', 'https://vercel.live'],
+      },
+    },
+  },
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
