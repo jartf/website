649aff6967dc3db0e2aa2f39926642eaf96dac90
Jarema <36879694+jartf@users.noreply.github.com>
2026-02-16 05:10:00 +0000

    remove unused dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
@@ -17,7 +16,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     react(),
-    mdx(),
   ],
 
   // Image optimization
