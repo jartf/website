5a9958d4417f1f4eae2e6a8b540215c9dd69a909
Jarema <36879694+jartf@users.noreply.github.com>
2026-03-15 16:54:32 +0000

    test: add Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
+
+const adapter = process.env.NETLIFY ? netlify() : vercel();
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jarema.me',
   trailingSlash: 'ignore',
   output: 'static',
-  adapter: vercel(),
+  adapter,
 
   integrations: [
     tailwind({
